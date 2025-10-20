4c15a2e1a01e6704b8f0f83f7980b6930609a7d1
rose <162016745+888Roses@users.noreply.github.com>
2025-10-20 17:51:43 +0200

    Init

--- src/main/java/net/rose/satchels/common/Satchels.java
+++ src/main/java/net/rose/satchels/common/Satchels.java
@@ -1,8 +1,14 @@
-package net.rose.satchels;
+package net.rose.satchels.common;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.util.Identifier;
 
 public class Satchels implements ModInitializer {
+    public static final String MOD_ID = "satchels";
+
+    public static Identifier identifier(String path) {
+        return Identifier.of(MOD_ID, path);
+    }
 
     @Override
     public void onInitialize() {
--- src/main/java/net/rose/satchels/data/SatchelsDataGenerator.java
+++ src/main/java/net/rose/satchels/data/SatchelsDataGenerator.java
@@ -1,4 +1,4 @@
-package net.rose.satchels.client;
+package net.rose.satchels.data;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
