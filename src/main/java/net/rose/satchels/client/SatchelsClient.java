15a619c712cf43c6fca8aa50b1b20e1a922e2250
rose <162016745+888Roses@users.noreply.github.com>
2025-10-20 17:51:26 +0200

    Initial commit

---
+++ src/main/java/net/rose/satchels/client/SatchelsClient.java
@@ -0,0 +1,10 @@
+package net.rose.satchels.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SatchelsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/net/rose/satchels/common/Satchels.java
@@ -0,0 +1,10 @@
+package net.rose.satchels;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Satchels implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
